ae526743979635dff52cceea6b4cfd1d4cfa641b
Bibek Ghimire <bibekghimire773@gmail.com>
2025-01-29 08:57:36 +0545

    list indexing

--- first.py
+++ first.py
@@ -34,7 +34,28 @@ a="string"
 # print(a[0:3])#0 1 2 index ko print hunxa
 # print(a[3:])# last ko 3 index ko print hunxa
 # # print(a[::3])
-print(len(a))
-print(a.count('s'))
-print(a.capitalize())
-print(a[-1])# reverse order ma first index
+# print(len(a))
+## print(a.count('s'))
+# print(a.capitalize())
+# print(a[-1])# reverse order ma first index
+# print(a.find('ng'));
+
+
+### escape sequence
+
+# a="bibek \n \t \' \\"
+# print(a)
+
+# print("hello", end="")#by default last ma \n hunthyo end le khali string rakhi diyo 
+# print("world")
+# sum=int(6)+int(6)
+# print("your sum is :" + str(sum))
+# print(f'your sum is :{sum}')
+# a=(input("Enter your name"))
+# print("Good morning "+ a)
+# #OR
+# print(f'hello {a} \n')
+l1=[1,2,3,4,'6']
+print(l1)
+print(l1[1])
+print(l1[:3])
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+a=(input("Enter your name"))
+# print("Good morning "+ a)
+# #OR
+# print(f'hello {a} \n')
