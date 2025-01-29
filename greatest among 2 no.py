de72000bc9279f9623ecca7111fb983d1d2b2f75
Bibek Ghimire <bibekghimire773@gmail.com>
2025-01-29 08:06:28 +0545

    conditional statement if, elif, else

--- greatest among 2 no.py
+++ greatest among 2 no.py
@@ -2,6 +2,8 @@ a=int(input("enter a number"))
 b=int(input("enter a number"))
 if a>b:
     print(a," is greater")
+elif a==0 or b==0:
+    print("either a or b is zero")
 
 else:
     print(b ," is greater")
