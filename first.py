3e360884754976376beed0968c304e7fa794df30
Bibek Ghimire <bibekghimire773@gmail.com>
2025-01-30 09:47:43 +0545

    function

--- first.py
+++ first.py
@@ -193,10 +193,4 @@ a="string"
 # b=int(input("enter a number"))
 # result = add(a, b)
 # print(result)
-def abc(p, t, r=int(10)):
-              print(f"intrest is {(p*t*r)/100}")
-p =int(input("enter principle val"))
-t =int(input("enter time val"))
-r =int(input("enter rate val"))
-abc(p, t, r)
-abc(p, t)
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
- palindrome
+def abc(p, t, r=int(10)):
+#               print(f"intrest is {(p*t*r)/100}")
+# p =int(input("enter principle val"))
+# t =int(input("enter time val"))
+# r =int(input("enter rate val"))
+# abc(p, t, r)
+# abc(p, t)
