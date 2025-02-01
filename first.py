5fcb2af83c0f2e478859a0fb3c9a965392a762f1
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-01 09:28:56 +0545

    json file created

--- first.py
+++ first.py
@@ -421,11 +421,23 @@ sum(a, b, c) """
 # sum=reduce(lambda x,y:x+y, number)
 # print(f"Sum is {sum}")  
 """file haldeling"""
-file=open("example.txt","w")
-file.write("hello world")
-file.close()
+# file=open("example.txt","w")
+# file.write("hello world")
+# file.close()
 
-file= open("example.txt","r")
-content= file.read()
-print(content)
-file.close()
+# file= open("example.txt","r")
+# content= file.read()
+# print(content)
+# file.close()
+
+
+import json
+dict={
+    "name": "",
+    "age": 10
+}
+with open("text.json","w")as f:
+    json.dump(dict,f)
+with open("text.json","r")as f:
+    loaded=json.load(f)
+print(loaded)
