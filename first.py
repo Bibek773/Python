422899f616b6f726ac7e611a2f6797eeaee5f498
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-01 07:14:24 +0545

    try except

--- first.py
+++ first.py
@@ -286,21 +286,30 @@ print(a+b)"""
 #         print("invalid choice")
 """inporting a library"""
 ##rock sissor paper 
-from random import randint as r
+# from random import randint as r
 
-def main(temp, user):
-    if temp == user:
-        print("It's a draw")
-    elif (temp == 'r' and user == 's') or (temp == 's' and user == 'p') or (temp == 'p' and user == 'r'):
-        print("You lose")
-    else:
-        print("You win")
-while(1):
-    play=input("enter p to play")
-    if play=='p':
-        game = ['r', 'p', 's']
-        temp = game[r(0, 2)]
-        user = input("Enter r for rock, s for scissors, p for paper: ")
-        main(temp, user)
-    else:
-        break
+# def main(temp, user):
+#     if temp == user:
+#         print("It's a draw")
+#     elif (temp == 'r' and user == 's') or (temp == 's' and user == 'p') or (temp == 'p' and user == 'r'):
+#         print("You lose")
+#     else:
+#         print("You win")
+# while(1):
+#     play=input("enter p to play")
+#     if play=='p':
+#         game = ['r', 'p', 's']
+#         temp = game[r(0, 2)]
+#         user = input("Enter r for rock, s for scissors, p for paper: ")
+#         main(temp, user)
+#     else:
+#         break
+"""try and except"""
+# try:
+#     num1= int(input("Enter a number"))
+#     num2= int(input("Enter a number"))
+#     result=num1/num2
+#     print("Result:", result)
+# except Exception as e:
+#     print(e)
+    
