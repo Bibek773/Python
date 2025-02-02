21a5bff5849c5eba17baa18ec17f717fd2877e8a
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-02 08:03:54 +0545

    create a list and append object in the list

--- oop_in_py.py
+++ oop_in_py.py
@@ -1,19 +1,45 @@
-class Student:
+# class Student:
+#     def __init__(self, fname, lname, age, address):
+#         self.name= fname
+#         self.lname= lname
+#         self.age =age
+#         self.address= address
+
+#     def get_name(self):
+#         return self.name + ' ' + self.lname
+#     def update_address(self, address):
+#         self.address= address
+# student1.update_address=
+# student1= Student('bibek',"ghimire", 19,'kathmandu')
+# student2= Student('bg', "g",20,'lalitpur')
+# print(student1.get_name())
+# print(student2.name)
+
+# #class list 
+#     # def __init__(self, numbers):
+#     #     self.numbers=numbers
+#     # def append():
+#     #     pass"""
+student=[]
+class Stu:
     def __init__(self, name, age, address):
         self.name= name
-        self.age =age
+        self.age= age 
         self.address= address
 
-    def get_name(self):
-        return self.name
+n=int(input("Enter the total no of student: "))
+for i in range(n):
     
-student1= Student('bibek', 19,'kathmandu')
-student2= Student('bg', 20,'lalitpur')
-print(student1.get_name())
-print(student2.name)
+    name=input(f"enter name of {i+1}th student: ")
+    age=input(f"enter age of {i+1}th student: ")
+    address=input(f"enter address of {i+1}th student: ")
 
-#class list 
-    # def __init__(self, numbers):
-    #     self.numbers=numbers
-    # def append():
-    #     pass
+    std = Stu(name, age, address)
+    student.append(std)
+new=input("Enter the name of student to change address: ")
+for stu in student:
+    if stu.name==new:
+        stu.address=input("Enter new address: ")
+for stu in student:
+    print(f"Name:{stu.name}\n Age: {stu.age}\n Address:{stu.address}")
+
