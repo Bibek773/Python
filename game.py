47b51ea29a92f422fbbf161a436f93fa031a796a
Bibek Ghimire <bibekghimire773@gmail.com>
2025-02-03 12:26:58 +0545

    Guess the correct number

---
+++ game.py
@@ -0,0 +1,16 @@
+import random
+
+random_num=random.randint(1,20)
+
+for i in range(0,4):
+    userinput=int(input(f"\t Guess the number\t\t\t\t{4-i} ❤️:\n\t"))
+    if random_num==userinput:
+        print("\n\t Congratulation You win!!!!!!!")
+        break
+    elif random_num>userinput:
+        print("Entered number is smaller than actual number")
+    else:
+        print("Entered number is greater than actual number")
+
+else:
+    print(f"You lost the number was {random_num}")
